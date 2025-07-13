8e4d9b7b499bccfaec700486e4fc796cd968bf68
abdullmalikrahman <abdullmalikrahman@gmail.com>
2025-07-14 04:04:56 +0700

    .

--- src/components/elements/modulpembelajaran/modul/ProgressBar.jsx
+++ src/components/elements/modulpembelajaran/modul/ProgressBar.jsx
@@ -5,8 +5,8 @@ const ProgressBar = (props) => {
       <div className="w-full bg-gray-200 rounded-full h-2.5 mb-2">
         <div
           className="bg-blue-500 h-2.5 rounded-full"
-          style={{ width: `${value}%` }}
-        ></div>
+          style={{ width: `${value}%` }}>
+        </div>
       </div>
       <p className="text-xs text-gray-500 mb-3 text-right">
         {value}% Selesai
