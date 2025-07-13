fd33f1d916d9ad5f8304348e39a86812cb8b32b9
abdullmalikrahman <abdullmalikrahman@gmail.com>
2025-07-14 04:07:03 +0700

    Update CardButton.jsx

--- src/components/elements/modulpembelajaran/modul/CardButton.jsx
+++ src/components/elements/modulpembelajaran/modul/CardButton.jsx
@@ -1,7 +1,7 @@
 const CardButton = (props) => {
     const { variant } = props;
     return (
-        <button class="w-full btn-primary text-sm mt-auto bg-${variant}-500 hover:bg-${variant}-600">
+        <button class={`w-full btn-primary text-sm mt-auto bg-${variant}-500 hover:bg-${variant}-600}`}>
             Mulai Belajar
         </button>
     );
