f83e0688b61c35f28534e54d55438a4a5716559a
Marcellinus08 <mgsmarcel123@gmail.com>
2025-08-10 13:00:40 +0700

    Update SubModul.jsx

--- src/pages/member/SubModul.jsx
+++ src/pages/member/SubModul.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import SubmodulLayout from "@/components/member/layouts/submodul/SubmodulLayout";
 import TentangModul from "@/components/member/fragments/submodul/TentangModul";
-import SubModulSection from "@/components/member/fragments/submodul/SubModulSection";
+import SubModulSection from "@/components/member/fragments/submodul/SubmodulSection";
 import SidebarLeft from "@/components/member/layouts/homepage/SidebarLeft";
 
 const SubModul = () => {
